763577db812901483145ef173c189519ec5835f6
c0deWookiee <project.drift2@gmail.com>
2019-01-08 22:53:48 -0800

    Added commands to package.json

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 var app = require("express")();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname, "../src/index.html");
+});
+
+io.on("connection", socket => {
+  console.log("<===A USER CONNECTED===>");
 });
 
 http.listen(8080, () => {
