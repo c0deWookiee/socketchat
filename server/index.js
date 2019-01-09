6fed54e16e4eb16281c700a8ccfcc7cfef2c17b0
c0deWookiee <project.drift2@gmail.com>
2019-01-09 14:34:42 -0800

    Static Files served!

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-var app = require("express")();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const path = require("path");
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname, "../src/index.html");
-});
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/../build"));
 
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../build/index.html"));
+// });
+
+const Server = require("http").Server(app);
+Server.listen(8080, () => console.log("<=====LISTENING ON PORT 8080====>"));
+
+const io = require("socket.io")(Server);
 io.on("connection", socket => {
   console.log("<===A USER CONNECTED===>");
-});
-
-http.listen(8080, () => {
-  console.log("<=====LISTENING ON PORT 7777====>");
+  socket.on("disconnect", () => {
+    console.log("===>A USER DISCONNECTED<===");
+  });
 });
