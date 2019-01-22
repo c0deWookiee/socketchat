138c3ed0cb45ecb8911ea135eda85797f8a39e27
Anthony <anthonyjquattrocchi@gmail.com>
2019-01-21 20:18:40 -0800

    More clearly delineated CSS classNames in <Messages />. Will no longer show your username on your own client.

--- src/Components/Messages.jsx
+++ src/Components/Messages.jsx
@@ -3,12 +3,25 @@ import DirectMessage from "./DirectMessage.jsx";
 import "../index.scss";
 
 export default function Messages(props) {
+  const showUsername = () => {
+    if (props.username === props.client) {
+      return <div />;
+    } else {
+      return (
+        <p onClick={() => props.privateMessage(props.id)} className="username">
+          {props.username}
+        </p>
+      );
+    }
+  };
   return (
-    <div className="messages">
+    <div className="feed">
       <div className={props.username === props.client ? "right" : "left"}>
-        <p onClick={() => props.privateMessage(props.id)}>{props.username}</p>
-        <h3>{props.message}</h3>
-        <p>{props.room}</p>
+        {showUsername}
+        <h3 div className="message">
+          {props.message}
+        </h3>
+        <p className="room-message">{props.room}</p>
       </div>
     </div>
   );
---
+++ src/Components/Username.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import DirectMessage from "./DirectMessage.jsx";
+import "../index.scss";
+
+export default function Username(props) {
+  return <div />;
+}
