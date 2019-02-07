1849fb21b9d556f1b422255448b1dfc93dbe0536
Daniel Pham <project.drift@gmail.com>
2019-02-06 22:41:14 -0800

    database designed and running

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS users
 DROP TABLE IF EXISTS rooms
 DROP TABLE IF EXISTS messages
 
-create table users (
-    user_id serial PRIMARY_KEY, 
+create table clients (
+    client_id serial PRIMARY_KEY, 
     username varchar(20), 
     password varchar(14), 
     email varchar(50), 
@@ -11,7 +11,7 @@ create table users (
     avatar varchar(250));
 
 create table rooms (
-    room_id serial PRIMARY_KEY,
+    rooms_id serial PRIMARY_KEY,
     room varchar(25));
 
 create table messages (
@@ -21,5 +21,12 @@ create table messages (
     room varchar(25),
     date_sent current_time,
 );
-inner join users from rooms
+
+create table messages_rooms (
+    message_id int references messages (message_id) ON UPDATE CASCADE ON DELETE CASCADE
+,   rooms_id int references rooms (rooms_id) ON UPDATE CASCADE ON DELETE CASCADE
+, CONSTRAINT rooms_messages_pkey PRIMARY_KEY (rooms_id, messages_id)
+)
+
+
 
