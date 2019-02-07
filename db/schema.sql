8a9458bddade316cf4e3568c0126f9a6aabfa0cc
Daniel Pham <project.drift@gmail.com>
2019-02-07 00:00:31 -0800

    fixed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,32 +1,34 @@
-DROP TABLE IF EXISTS users
-DROP TABLE IF EXISTS rooms
-DROP TABLE IF EXISTS messages
+DROP TABLE clients;
+DROP TABLE rooms;
+DROP TABLE messages;
 
 create table clients (
-    client_id serial PRIMARY_KEY, 
+    client_id serial PRIMARY KEY, 
     username varchar(20), 
     password varchar(14), 
     email varchar(50), 
     rooms text[], 
-    avatar varchar(250));
+    avatar varchar(250)
+);
 
 create table rooms (
-    rooms_id serial PRIMARY_KEY,
-    room varchar(25));
+    rooms_id serial PRIMARY KEY,
+    room varchar(25)
+);
 
 create table messages (
-    message_id serial PRIMARY_KEY,
+    message_id serial PRIMARY KEY,
     username varchar(20),
     message varchar(900),
     room varchar(25),
-    date_sent current_time,
+    date_sent timestamp
 );
 
-create table messages_rooms (
+create table chatdb.messages_rooms (
     message_id int references messages (message_id) ON UPDATE CASCADE ON DELETE CASCADE
 ,   rooms_id int references rooms (rooms_id) ON UPDATE CASCADE ON DELETE CASCADE
-, CONSTRAINT rooms_messages_pkey PRIMARY_KEY (rooms_id, messages_id)
-)
+, CONSTRAINT rooms_messages_pkey PRIMARY KEY (rooms_id, message_id)
+);
 
 
 
